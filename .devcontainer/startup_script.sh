3a567a11a57ec093f40066e3d755298a515e46c6
Sam <8408317+samgabrail@users.noreply.github.com>
2023-02-17 20:41:53 +0000

    check pwd

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+whoami
+pwd
+cd .devcontainer
 docker build -t jenkins_container .
 docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkins_container
 echo 'Go to the Ports tab where you will find the link to access the Jenkins UI on port 8080 \n Here is your Initial Admin Password to access Jenkins: \n' && sudo cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword
