dfee327f94c6251299fdf477e39c50872016af04
Sam <8408317+samgabrail@users.noreply.github.com>
2023-02-17 21:44:16 +0000

    nicer output

--- .devcontainer/startup_script.sh
+++ .devcontainer/startup_script.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/bash
 # The Docker image below has the latest Jenkins image and the Terraform, Vault, and Ansible versions are found in the Dockerfile in this directory.
 docker run -d -v jenkins_home:/var/jenkins_home -p 8080:8080 -p 50000:50000 samgabrail/jenkins-tf-vault-ansible:latest
-echo "Go to the Ports tab where you will find the link to access the Jenkins UI on port 8080"
-echo "Run the following command to get your initial admin password to access Jenkins:"
-echo "sudo cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword"
+echo "✅ Go to the Ports tab where you will find the link to access the Jenkins UI on port 8080"
+echo "...Retrieving your initial admin password"
+sleep 7
+echo "✅ Below are your initial admin password to access Jenkins:"
+sudo cat /var/lib/docker/volumes/jenkins_home/_data/secrets/initialAdminPassword
 
 
 # If you would like to use newer versions of Terraform, Vault, and Ansible, then uncomment the 2 lines below and update the Dockerfile in this directory, this will take about 5 minutes to build the image.
